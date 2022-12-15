d3f5f59acb190bf6677496da6ef8c8ca538c16f1
O. Barnes <104699270+OMARI777@users.noreply.github.com>
2022-12-14 19:24:32 -0700

    Message Correction

--- resources/app.js
+++ resources/app.js
@@ -1,8 +1,8 @@
 function randomMessage() {
-    positive = ['U will be Wealthy!!','Be Cheerful', 'Be Confident','Love All',];
+    positive = ['U Will Be Wealthy!!','Be Cheerful', 'Be Confident','Love All',];
     healthy = ['Work Hard!!','Athletic', 'Active','Stay Hungry','Think Big'];
-    wealthy = ['Do it scared','U will be Rich','Cash Money For You','Move Mountains',];
-    blessed = ['Look how far you have come!!','Good Things To Come!!','You Will Be Blessed!',];
+    wealthy = ['Do It Scared','U Will Be Rich','Cash Money For You','Move Mountains',];
+    blessed = ['Look How Far You Have Come!','Good Things To Come!!','You Will Be Blessed!',];
 
     const randomize = (array) => {
     return array[Math.floor(Math.random()*array.length)];
