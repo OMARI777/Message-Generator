fa33112215ed87ddebb25e71b5065021bd6cd187
O. Barnes <104699270+OMARI777@users.noreply.github.com>
2022-12-14 17:27:53 -0700

    3rd change

--- resources/app.js
+++ resources/app.js
@@ -1,16 +1,16 @@
 function randomMessage() {
     positive = ['U will be Wealthy!!','Be Cheerful', 'Be Confident',];
     healthy = ['Work Hard!!','Athletic', 'Active',];
-    wealthy = ['Do it scared','U will be Rich','Cash Money',];
-    blessed = ['Look how far you have come!!','Good Things To Come!!',];
+    wealthy = ['Do it scared','U will be Rich','Cash Money For You',];
+    blessed = ['Look how far you have come!!','Good Things To Come!!','You Will Be Blessed!',];
 
     const randomize = (array) => {
     return array[Math.floor(Math.random()*array.length)];
 };
 
-let mealMaker = randomize(positive) + ", " + randomize(healthy) + ", " +  randomize(wealthy) + ", " + randomize(blessed);
+let fortuneCookie = randomize(positive) + ", " + randomize(healthy) + ", " +  randomize(wealthy) + ", " + randomize(blessed);
 
-document.getElementById("message").innerHTML = mealMaker;
+document.getElementById("message").innerHTML = fortuneCookie;
 }
   
   
